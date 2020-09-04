187967bbab63457b817c2df38c41ef778906764c
rodrigojimenez <rodrigojimenez@sqdm.com>
2020-09-04 10:50:08 -0500

    configuracion para heroku

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+app.use(express.static(__dirname+'/angularapp'));
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
